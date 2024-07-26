de053c0261ba21d808573629e58714ef1c35e769
William Chen <williamchen120@gmail.com>
2024-07-26 12:51:58 -0700

    Functional Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useRef } from 'react'
 import './App.css'
-import { IoIosSearch } from "react-icons/io";
-import { BiSolidNavigation } from "react-icons/bi";
-import Clock from 'react-clock';
+import { ToggleSlider }  from "react-toggle-slider"
 
 function fToC(Temp) {
   return (Temp - 273.15) * (9 / 5) + 32
@@ -11,19 +9,24 @@ function fToC(Temp) {
 function App() {
   
   const inputRef = useRef()
+  const [isOpen, setIsOpen] = useState('False')
   const [temperatureF, setTemperatureF] = useState('--')
   const [temperatureC, setTemperatureC] = useState('--')
+  const [fCSwitch, setFCSwitch] = useState('True')
   const [city, setCity] = useState('')
 
   const search = async(location) => {
+    
+    if (!confirm(`Are you sure you want to select: ${location}?`)) {
+      return
+    }
 
     const key = import.meta.env.VITE_APP_API_KEY
-    console.log(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`)
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`);
     const data = await response.json();
 
     if (!response.ok){
-      alert("Enter Valid City")
+      alert("City is not a valid city")
     }
 
     setTemperatureF(Math.round(fToC(data.main.temp)))
@@ -31,24 +34,25 @@ function App() {
     setCity(location)
   }
 
-    return (
-      <>
-      <form>
-        <input ref={inputRef} type="text" placeholder="Search..."></input>
-        <button type="button" onClick={() => search(inputRef.current.value)}>Enter</button>
-      </form>
-        <div id="card">
-          <p>
-            <strong>{city}</strong>
-          </p>
-          <p>
-            <strong>{temperatureF} °F</strong>
-          </p>
-        </div>
-      </>
-    ) 
+  return (
+    <>
+    <form>
+      <input ref={inputRef} type="text" placeholder="Search..."></input>
+      <button type="button" onClick={() => search(inputRef.current.value)}>Enter</button>
+    </form>
+    <p id="side-note">Enter your city to find relevant weather information.</p>
+    <div id="card">
+      <p><strong>{city}</strong></p>
+      <p>{fCSwitch ? <strong>{temperatureF} °F</strong> : <strong>{temperatureC} °C</strong>}</p>
+      <div id="switch">
+        °F <ToggleSlider onToggle={state => setFCSwitch(!state)}/> °C
+      </div>
+      <p id="side-note">Click on me to change units!</p>
+    </div>
+    <div id="modal">
+    </div>
+    </>
+  ) 
 }
 
 export default App
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    <footer>
+    Weather App Version 0.0.1 made by William Chen
+    </footer>
+  </React.StrictMode>
 )
 
 
